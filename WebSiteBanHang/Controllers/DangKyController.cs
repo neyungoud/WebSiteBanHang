2abb7ae5e1d8ec5926dd8381b3193465adebc763
Thang <ndthang9597@gmail.com>
2020-06-23 23:45:21 +0700

    Sua phan Dang Ky

--- WebSiteBanHang/Controllers/DangKyController.cs
+++ WebSiteBanHang/Controllers/DangKyController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace WebSiteBanHang.Controllers
 {
-    [Authorize(Roles = "DangKy")]
+    [Authorize(Roles = "DangNhap")]
     public class DangKyController : Controller
     {
         //
