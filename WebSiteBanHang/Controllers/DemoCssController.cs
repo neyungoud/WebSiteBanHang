480e59fb267096b269a7c69becd08d444f5fdff7
neyungoud <neyungoud@DESKTOP-NJ53986>
2020-06-24 11:04:34 +0700

    Duong update controller

--- WebSiteBanHang/Controllers/DemoCssController.cs
+++ WebSiteBanHang/Controllers/DemoCssController.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace WebSiteBanHang.Controllers
-{
-    public class DemoCssController : Controller
-    {
-        //
-        // GET: /DemoCss/
-        public ActionResult Demo()
-        {
-            return View();
-        }
-	}
-}
+﻿
