769fb6a8fc286b142cb53358bf64365aba9345dc
neyungoud <neyungoud@DESKTOP-NJ53986>
2020-06-24 11:06:03 +0700

    Done CSS controller

--- WebSiteBanHang/Controllers/DemoCssController.cs
+++ WebSiteBanHang/Controllers/DemoCssController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteBanHang.Controllers
+{
+    public class DemoCssController : Controller
+    {
+        //
+        // GET: /DemoCss/
+        public ActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
